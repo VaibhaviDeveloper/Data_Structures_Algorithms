e5650d03fdd6cd07f1c98f3c4dd9a65340f10b45
VaibhaviDeveloper <nayakvaibhavi.nvk@gmail.com>
2026-08-13 11:29:13 +0530

    Implemented insert at end operation for linked list

--- LinkedList.py
+++ LinkedList.py
@@ -22,7 +22,7 @@ def traverse():
 insertAtTop(10)
 insertAtTop(20)
 insertAtTop(30)
-traverse()
+ # traverse()
 
 def insertAtEnd(data):
   global head
@@ -36,4 +36,30 @@ def insertAtEnd(data):
     curr.next=nn
 
 insertAtEnd(40)
+#traverse()
+
+def insertAtPos(data,pos):
+  global head
+  nn = Node(data)
+  if head is None:
+    head=nn
+  elif pos==0:
+    nn.next=head
+    head=nn
+  else:
+    curr=head
+    i=0
+    while (i<pos-1 and curr is not None and curr.next is not None):
+      curr=curr.next
+      i+=1
+    nn.next=curr.next
+    curr.next=nn
+
+insertAtPos(20,1)
+insertAtPos(90,0)
+insertAtPos(100,100)
+insertAtPos(100,0)
 traverse()
+
+    
+     
