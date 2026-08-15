89615010ab7a09cc9ad568d3cbf42ca01dd4d63c
VaibhaviDeveloper <nayakvaibhavi.nvk@gmail.com>
2026-08-15 18:26:09 +0530

    Implemented solutions for sorting 0 and 1

---
+++ TiwariQs.py
@@ -0,0 +1,21 @@
+a=[0,0,1,0,2,2,0,1,1,0,0,2]
+
+def Sorting(a):
+    n=len(a)
+    i=0
+    j=n-1
+    while(i<=j):
+        if(a[i]==0):
+            i+=1
+        elif(a[j]==1 or a[j]==2):
+            j-=1
+        else:
+            a[i],a[j]=a[j],a[i]
+            i+=1
+            j-=1
+    print(i)
+    print(j)
+    
+Sorting(a)
+print(a)
+
